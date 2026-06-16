0648f0faa09b7e664872c3b1bf89c58abc1c61bf
mkjabed <jabed5564@gmail.com>
2026-06-16 15:53:13 +0600

    Solve Problem 2

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,23 @@
+pattern = input()
+text = input()
+d = int(input())
 
+def hamming_distance(a, b):
+    distance = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            distance+=1
+            
+    return distance
+
+text_length = len(text)
+k = len(pattern)
+positions = []
+
+for i in range(text_length - k + 1):
+    substring = text[i:i+k]
+    if hamming_distance(substring, pattern) <= d:
+        positions.append(i)
+
+print(*positions)
+        
