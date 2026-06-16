c551341f9e8ba7e0f653e1c815e84e66c7834224
mkjabed <jabed5564@gmail.com>
2026-06-16 11:18:50 +0600

    Solve problem 1

--- Task 2/Problem1.py
+++ Task 2/Problem1.py
@@ -2,10 +2,11 @@ text = input()
 pattern = input()
 
 count = 0 
+pattern_length = len(pattern)
+text_length = len(text)
 
-for base in text:
-    if base == pattern:
-        count+=1 
-    
+for i in range(text_length - pattern_length + 1):
+    if pattern == text[i:i+pattern_length]:
+        count += 1
+        
 print(count)
-
--- Task 2/tempCodeRunnerFile.py
+++ Task 2/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-d
+ting Index: {i}")
+    if pattern =
